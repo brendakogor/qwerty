d6d7ca468d6238182ce2a6f5832934f54aff308a
brendakogor <ml_brends@hotmail.com>
2018-04-15 16:31:36 -0500

    primer commit
    primera parte del proyecto

---
+++ main.c
@@ -0,0 +1,17 @@
+//Brenda Méndez López 
+//Programa 1.1: recibe 2 datos y devuelve su promedio.
+
+#include <conio.h>
+#include <stdio.h>
+
+int main (){
+    float dato1, dato2, dato3, promedio;
+    printf("Bienvenido.\n\n");
+    printf("Este programa obtiene el promedio de dos numeros. \n");
+    printf("Teclee sus datos separados por un espacio \n");
+    scanf("%f %f", &dato1, &dato2);
+    promedio = (dato1 + dato2 ) / 2;
+    printf("El promedio es %4.1f ", promedio);
+    printf("\n\nOprima cualquier tecla para terminar...");
+    getch();   
+}
